46e7ef219e86154399338a80ed47cdf009db36dc
krzysztoftobiasz <48628424+krzysztoftobiasz@users.noreply.github.com>
2019-06-25 22:58:22 +0200

    nie wyszlo

---
+++ FiguresLib/FiguresLib/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace FiguresLib
+{
+    abstract public class Figure
+    {
+        public string Label { get; set; }
+        private static int counter = 0;
+        public Color Color { get; set; }
+        public static int FRACTIONAL_DIGITS { get; protected set; }
+
+        public Figure()
+        {
+            counter++;
+            this.Color = Color.Black;
+            Label = $"{GetType().Name} #{counter}";
+        }
+
+        abstract public void Draw();
+        public override string ToString()=>$"Figure:{Label}";
+
+        
+        
+    }
+    public class Point : Figure, IEquatable<Point>
+    {
+        // fields
+        public readonly double X, Y; //immutability
+
+        // constructor
+        public Point(double x = 0, double y = 0)
+        {
+            X = Math.Round(x, Figure.FRACTIONAL_DIGITS);
+            Y = Math.Round(y, Figure.FRACTIONAL_DIGITS);
+            Color = Color.Blue;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("drawing: " + $"Point({X}, {Y}), {Color}");
+        }
+
+        public override string ToString() => $"Point({X},{Y})";
+
+        #region implementation of IEquatable<Point>
+        // https://docs.microsoft.com/en-us/dotnet/api/system.object.equals?view=netstandard-2.0
+        public bool Equals(Point other) => (other is null) ? false : (this.X == other.X && this.Y == other.Y);
+        public override bool Equals(Object obj)
+        {
+            if (!this.GetType().Equals(obj.GetType())) return false;
+
+            return this.Equals((Point)obj);
+        }
+
+        public override int GetHashCode() => unchecked(((int)X << 2) ^ (int)Y);
+
+        static public bool operator ==(Point p1, Point p2)
+        {
+            if (p1 is null && p2 is null) return true;
+            if (p1 is null && !(p2 is null)) return false;
+
+            //p1 != null
+            return p1.Equals(p2);
+        }
+        static public bool operator !=(Point p1, Point p2) => !(p1 == p2);
+        #endregion
+    }
+}
---
+++ FiguresLib/FiguresLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("FiguresLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FiguresLib")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("479612d8-e09e-462a-a8ba-5e6490063a12")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
